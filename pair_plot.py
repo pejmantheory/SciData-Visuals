29b9ca5b36509e092a4562d78d80f78adcecc4c1
Pejman Haghighatnia <pejmanhnia@gmail.com>
2024-05-22 02:28:04 -0700

    Upload iris.data.txt

--- pair_plot.py
+++ pair_plot.py
@@ -1,8 +1,10 @@
 import seaborn as sns
+import pandas as pd
 import matplotlib.pyplot as plt
 
-# Load the iris dataset
-iris = sns.load_dataset("iris")
+# Load the dataset from a local file
+column_names = ["sepal_length", "sepal_width", "petal_length", "petal_width", "species"]
+iris = pd.read_csv("iris.data.txt", header=None, names=column_names)
 
 # Create the pair plot
 sns.pairplot(iris, hue='species', palette='Dark2')
